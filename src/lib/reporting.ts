e3f862acdd2797af3d64528ac0dc720da7cfb804
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-03-11 17:52:39 +0100

    Update reporting.ts

--- src/lib/reporting.ts
+++ src/lib/reporting.ts
@@ -228,6 +228,10 @@ export const soil_moisture = async (endpoint: Zh.Endpoint, overrides?: Reporting
     const p = payload("measuredValue", 10, repInterval.HOUR, 100, overrides);
     await endpoint.configureReporting("msSoilMoisture", p);
 };
+export const windSpeed = async (endpoint: Zh.Endpoint, overrides?: Reporting.Override) => {
+    const p = payload("measuredValue", 10, repInterval.HOUR, 100, overrides);
+    await endpoint.configureReporting("msWindSpeedMeasurement", p);
+};
 export const acFrequency = async (endpoint: Zh.Endpoint, overrides?: Reporting.Override) => {
     const p = payload("acFrequency", 5, repInterval.MINUTES_5, 10, overrides);
     await endpoint.configureReporting("haElectricalMeasurement", p);
